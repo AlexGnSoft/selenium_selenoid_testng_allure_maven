f659b84fb5951b8fb39904b0138dac851ac7be1e
Alex Gnuskin <agnuskin@optimizerx.com>
2022-12-14 18:42:54 +0000

    testing after jenkins run failed tests

--- src/main/java/com/carespeak/domain/steps/imp/prod/ProdProgramSteps.java
+++ src/main/java/com/carespeak/domain/steps/imp/prod/ProdProgramSteps.java
@@ -78,8 +78,9 @@ public class ProdProgramSteps implements ProgramSteps {
             programsPage.waitFor(() -> !dashboardPage.getCurrentUrl().equals(url), false);
         }
         dashboardPage.headerMenu.programsMenuItem.click();
-        waitFor(()-> programsPage.isOpened());
+        //waitFor(()-> programsPage.isOpened());
         //programsPage.searchClient.search(clientName);
+        programsPage.addProgramButton.isDisplayed();
         programsPage.addProgramButton.click();
         programSettingsPage.programNameInput.enterText(programName);
         programSettingsPage.programAccessDropDown.select(programAccess.getValue());
